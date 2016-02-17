010e43a6e2b284fc73df235cd59f81e259347b8e
ddaniels <ddaniels@palantir.com>
2016-02-17 10:34:46 -0800

    movie colors fixzed

--- poster/movie_color.R
+++ poster/movie_color.R
@@ -1,8 +1,10 @@
 library(ggplot2)
 movie <- c('Bridge of Spies', 'Mad Max: Fury Road', 'Room','The Revenant','Brooklyn','Spotlight',
            'The Big Short','The Martian')
+
 cols <- c('#CC0000', '#FFCC33','#3399FF','#999999','#336699','#FF9933','#9933FF','#CC6600') 
-movie_color <- data.frame(cbind(movie,cols))
+movie_color <- data.frame(cbind(movie,movie_colors))
+levels(movie_color$movie) <- movie
 
 exampleColorPlot <- function () {
   ggplot(movie_color,aes(movie)) + geom_bar(fill = cols)
